f1990e5e3b4b22c3bbe0cd6cd9f968a42e736888
dangminh214 <dangminh214@gmail.com>
2024-03-31 14:54:27 +0200

    using useRef, create dialog and add ResultModal, TimerChallenge to components. Forwarrding ref from other component

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,42 @@
+import { useState, useRef } from "react";
+import ResultModel from "./ResultModel";
+
+export default function TimerChallenge({title, targetTime}) {
+  const timer = useRef();
+  const dialog = useRef();
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpreird] = useState(false); 
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpreird(true)
+      dialog.current.showModal();
+    }, targetTime * 1000);
+    setTimerStarted(true)
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
+  return (
+    <>
+      <ResultModel ref = {dialog} targetTime = {targetTime} result="lost"/>
+      <section className ="challenge">
+        <h2>{title}</h2>
+        {timerExpired && <p>You lost!</p>}
+        <p className="challenge-time">
+          {targetTime} seconds { targetTime > 1 ? 's':''}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'} Challenge
+          </button>
+        </p>
+        <p className = {timerStarted ? 'active': undefined}>
+          {timerStarted ? 'Time is running...' : 'Timer inactive'}
+        </p>
+      </section>
+    </>
+  )
+}
