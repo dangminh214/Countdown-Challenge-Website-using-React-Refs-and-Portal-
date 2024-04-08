27996dca89cccbe1ba47c4fbb30b5cfa770419e3
dangminh214 <dangminh214@gmail.com>
2024-04-08 09:37:02 +0200

    fix typo chanllenge-time result

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -41,7 +41,7 @@ export default function TimerChallenge({title, targetTime}) {
       <section className ="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
-          {targetTime} seconds { targetTime > 1 ? 's':''}
+          {targetTime} second { targetTime > 1 ? 's':''}
         </p>
         <p>
           <button onClick={timerIsActive ? handleStop : handleStart}>
